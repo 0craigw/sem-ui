8b3770580bd2011d4ecf124b7b6c248f4a1a43e6
Craig Williams <Craig.Williams@lic.co.nz>
2018-10-08 09:59:08 +1300

    Install semantic css package.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Button } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css';
+import { Button, Header, List, Segment } from 'semantic-ui-react'
 
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header>
-          Semantic UI examples
-        </header>
-        <div>
-          <a href="https://www.npmjs.com/package/semantic-ui-react">semantic-ui-react npm page</a>
-        </div>
-        <div>
-          <Button size='small' color='green'>
-              Button
-          </Button>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<Header as='h1'>
+					Semantic UI examples
+        		</Header>
+				<Segment>
+					<div>
+						Here's a list
+          			</div>
+					<List items={['Apples', 'Bananas', 'Oranges']} />
+				</Segment>
+				<div>
+					<Button size='small' color='green'>
+						Button
+          			</Button>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
